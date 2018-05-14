8902eb87eed2b6ff68907892ef8787e54faef6bf
林松賢 <stevenlin@linsongxiande-MacBook-Pro.local>
2018-05-14 23:35:42 +0800

    Initial Commit

---
+++ 垃圾車來了/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  垃圾車來了
+//
+//  Created by 林松賢 on 2018/5/14.
+//  Copyright © 2018年 林松賢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
